1854c5a5afb9395f76bf110a1f4d364a271cf6c2
xujinjia <1109415343@qq.com>
2025-07-09 14:25:46 +0800

    删除
    删除原有框架

--- src/main/java/com/modelevalplat/service/ModelService.java
+++
@@ -1,9 +0,0 @@
-package com.modelevalplat.service;
-
-import com.modelevalplat.model.ModelEvaluationResult;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface ModelService {
-    String uploadModel(MultipartFile file) throws Exception;
-    ModelEvaluationResult evaluateModel(String modelPath) throws Exception;
-}
