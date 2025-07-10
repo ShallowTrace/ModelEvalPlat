36b17d0674924484dac6964ab9143c6f99230289
xujinjia <1109415343@qq.com>
2025-07-10 15:16:02 +0800

    框架

--- src/main/java/com/ecode/ModelEvalPlat/ModelEvalPlatApplication.java
+++ src/main/java/com/ecode/ModelEvalPlat/ModelEvalPlatApplication.java
@@ -1,4 +1,5 @@
-package com.ecode.ModelEvalPlat;
+package main.java.com.ecode.ModelEvalPlat;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
