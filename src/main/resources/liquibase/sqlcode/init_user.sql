7ca9e91e7b536b6c3728fd6c5bdee492f28eeb32
CQU888 <2725939757@qq.com>
2025-08-14 17:51:36 +0800

    改造环境变量

--- src/main/resources/liquibase/sqlcode/init_user.sql
+++ src/main/resources/liquibase/sqlcode/init_user.sql
@@ -1,3 +1,24 @@
-INSERT INTO model_eval_plat.users (username, password, email, role, is_active, created_at, last_login_at)
-VALUES ('admin', '123456', '123@qq.com', 'admin', 1, '2025-08-12 16:24:50', '2025-08-12 16:24:52');
-
+INSERT INTO users (
+    username,
+    password,
+    email,
+    role,
+    is_active,
+    gender,
+    avatar_url,
+    department,
+    created_at,
+    last_login_at
+)
+VALUES (
+           'admin',
+           '123456',
+           '123@qq.com',
+           'admin',
+           1,
+           'male',
+           'https://example.com/avatar.jpg',
+           'Administration',
+           '2025-08-12 16:24:50',
+           '2025-08-12 16:24:52'
+       );
